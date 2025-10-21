f7b0e63ad3eea2ab787b02f6642e56a389905554
陈瑞生 <1115009958@qq.com>
2025-10-21 20:19:55 +0800

    Update nitro.json timestamp and remove obsolete assets
    - Updated the timestamp in nitro.json for better management.
    - Deleted obsolete JavaScript, CSS, and asset files from the dist/assets directory to streamline the build output and improve organization.
    - Cleaned up unnecessary files to enhance project maintainability.

--- dist/assets/route-view-DgRHdPow.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./route-view.vue_vue_type_script_setup_true_lang-DN6hexEh.js";import"./vue-CHyeelTQ.js";import"./index-DxV6Ufq3.js";import"./antd-CtyD9qUH.js";export{o as default};
