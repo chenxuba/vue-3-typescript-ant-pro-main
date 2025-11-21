b6b3674b027b53d3d4c04a8c9fb24959a3f6d561
陈瑞生 <1115009958@qq.com>
2025-11-21 14:54:14 +0800

    chore: update build artifacts and hash references

---
+++ dist/assets/organization-Bbw6HZQQ.js
@@ -0,0 +1,1 @@
+import{k as r}from"./index-PVXIpLUY.js";async function e(t){return r("/web/api/user/allOrgList",t,{customDev:!0})}export{e as g};
--- dist/assets/route-view-BLzIUp7X.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./route-view.vue_vue_type_script_setup_true_lang-CyWsYpIr.js";import"./vue-PnzzEkKE.js";import"./index-D0F-TMbH.js";import"./antd-Cme2pY03.js";export{o as default};
